c2b8c3b253f09170b19c241b68ab5f868e2f375d
JJ <jonjolivette777@aol.com>
2019-09-07 13:56:27 -0500

    added heroku port access

--- app.js
+++ app.js
@@ -30,6 +30,5 @@ app.use((err, req, res, next) => {
 	res.render('error');
 });
 
-app.listen(3000, () => {
-	console.log('The application is running on localhost:3000!');
-});
+//heroku port access and local access via port 3000
+app.listen(process.env.PORT || 3000);
